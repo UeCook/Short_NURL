cd719b6a1859145976793afd57640b64e2054df6
UeCook <145826518+UeCook@users.noreply.github.com>
2026-05-23 17:37:28 +0800

    Delete frontend directory

--- frontend/theme-init.js
+++
@@ -1,2 +0,0 @@
-/* Prevent FOUC: inline theme init before render */
-(function(){try{var t=localStorage.getItem("su_theme");if(t==="light")document.documentElement.classList.remove("dark");else if(!t||t==="dark")document.documentElement.classList.add("dark")}catch(e){}})();
